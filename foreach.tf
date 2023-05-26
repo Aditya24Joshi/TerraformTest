26db186b4fd6c2750c390b84b353e0347a83d1f8
Aditya24Joshi <joshi.aditya303@gmail.com>
2023-05-26 11:32:32 +0000

    updated foreach toset method

--- foreach.tf
+++ foreach.tf
@@ -1,10 +1,5 @@
 resource "azurerm_resource_group" "myrg" {
-for_each = {
-dcapp = "eastus"
-dcapp1 = "westus"
-dcapp2 = "centralindia"
-}
-name = "${each.key}-rg"
-location = each.value
-  
+for_each = toset(["eastus", "westus", "centralindia"])
+name = "${each.value}-rg"
+location = each.key
 }
