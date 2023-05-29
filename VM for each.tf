74bb3e4826e51281da906203a6496957ec55c090
Aditya24Joshi <joshi.aditya303@gmail.com>
2023-05-29 08:30:40 +0000

    updated pub ip config

--- VM for each.tf
+++ VM for each.tf
@@ -36,7 +36,7 @@ ip_configuration{
 name = "internal"
 subnet_id = azurerm_subnet.mysubnet.id
 private_ip_address_allocation = "Dynamic"
-public_ip_address_id = [azurerm_public_ip.myIP[each.key].id]
+public_ip_address_id = azurerm_public_ip.myIP[each.key].id
 }
 }
 
